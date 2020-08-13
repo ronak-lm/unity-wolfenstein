10f11d371d632270337b61a949a7b72bce73d98e
Ronak Manglani <ronak.manglani@gmail.com>
2020-08-14 00:20:11 +0530

    make door open and close animation

---
+++ Assets/Scripts/DoorOpenFirst.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpenFirst : MonoBehaviour
+{
+    public GameObject theDoor;
+
+    void OnTriggerEnter(Collider other)
+    {
+        theDoor.GetComponent<Animator>().Play("DoorOpen");
+        GetComponent<BoxCollider>().enabled = false;
+        StartCoroutine(CloseDoor());
+    }
+
+    IEnumerator CloseDoor()
+    {
+        yield return new WaitForSeconds(5);
+        theDoor.GetComponent<Animator>().Play("DoorClose");
+        GetComponent<BoxCollider>().enabled = true;
+    }
+}
