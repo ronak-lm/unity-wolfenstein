4e2cc0381f94d3d184cd6bce1707fdefeb07b0e7
Ronak Manglani <ronak.manglani@gmail.com>
2020-08-14 01:03:56 +0530

    add gun to scene and make pickup script

---
+++ Assets/Scripts/Weapons/HandgunPickup.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandgunPickup : MonoBehaviour
+{
+    public GameObject realHandgun;
+    public GameObject fakeHandgun;
+    public AudioSource handgunPickupSound;
+
+    void OnTriggerEnter(Collider other)
+    {
+        realHandgun.SetActive(true);
+        fakeHandgun.SetActive(false);
+        handgunPickupSound.Play();
+    }
+}
