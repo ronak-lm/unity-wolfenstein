140bf2531643990aad93fdca24c95ff9fb4bda91
Ronak Manglani <ronak.manglani@gmail.com>
2020-08-14 22:56:36 +0530

    make gun shooting script

---
+++ Assets/Scripts/Weapons/HandgunFire.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandgunFire : MonoBehaviour
+{
+    public GameObject theGun;
+    public GameObject muzzleFlash;
+    public AudioSource gunFireSound;
+
+    private bool isFiring;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1") && !isFiring)
+        {
+            StartCoroutine(FireHandgun());
+        }
+    }
+
+    IEnumerator FireHandgun()
+    {
+        isFiring = true;
+        theGun.GetComponent<Animator>().Play("HandgunFire");
+        muzzleFlash.SetActive(true);
+        gunFireSound.Play();
+
+        yield return new WaitForSeconds(0.05f);
+        muzzleFlash.SetActive(false);
+
+        yield return new WaitForSeconds(0.25f);
+        theGun.GetComponent<Animator>().Play("Default");
+        isFiring = false;
+    }
+}
