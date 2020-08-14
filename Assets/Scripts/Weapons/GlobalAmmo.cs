ffa4177376b06cfe5cf7e6edbbce42c8398138b8
Ronak Manglani <ronak.manglani@gmail.com>
2020-08-14 23:39:42 +0530

    make ammo pickup system

---
+++ Assets/Scripts/Weapons/GlobalAmmo.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GlobalAmmo : MonoBehaviour
+{
+    public static int handgunAmmo;
+    public GameObject ammoDisplay;
+
+    void Update()
+    {
+        ammoDisplay.GetComponent<Text>().text = "" + handgunAmmo;    
+    }
+}
---
+++ Assets/Scripts/Weapons/HandgunAmmoPickup.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandgunAmmoPickup : MonoBehaviour
+{
+    public GameObject ammoClip;
+    public AudioSource ammoPickupSound;
+
+    void OnTriggerEnter(Collider other)
+    {
+        ammoClip.SetActive(false);
+        ammoPickupSound.Play();
+
+        GlobalAmmo.handgunAmmo += 10;
+    }
+}
