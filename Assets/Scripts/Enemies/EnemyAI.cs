5e0cd8b0ebd1e4b3147d5b197a74e2ecdd548ed4
Ronak Manglani <ronak.manglani@gmail.com>
2020-08-16 01:14:29 +0530

    make script to shoot at player when in sight

---
+++ Assets/Scripts/Enemies/EnemyAI.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    public string hitTag;
+    public bool lookingAtPlayer = false;
+    public GameObject theEnemy;
+
+    void Update()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit))
+        {
+            hitTag = hit.transform.tag;
+        }
+        if (hitTag == "Player")
+        {
+            theEnemy.GetComponent<Animator>().Play("FirePistol");
+            lookingAtPlayer = true;
+        }
+        else
+        {
+            theEnemy.GetComponent<Animator>().Play("Idle");
+            lookingAtPlayer = false;
+        }
+    }
+}
