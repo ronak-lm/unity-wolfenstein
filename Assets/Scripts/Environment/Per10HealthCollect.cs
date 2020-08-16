801d1982bf2a73e591c477607e986937fcdc0ce2
Ronak Manglani <ronak.manglani@gmail.com>
2020-08-17 00:35:44 +0530

    add small health pickup

---
+++ Assets/Scripts/Environment/Per10HealthCollect.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Per10HealthCollect : MonoBehaviour
+{
+    public AudioSource collectSound;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (GlobalHealth.healthValue >= 91)
+        {
+            GlobalHealth.healthValue = 100;
+        }
+        else
+        {
+            GlobalHealth.healthValue += 10;
+        }
+        collectSound.Play();
+        GetComponent<BoxCollider>().enabled = false;
+        this.gameObject.SetActive(false);
+    }
+}
