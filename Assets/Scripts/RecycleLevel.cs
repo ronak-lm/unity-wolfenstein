92e3f469673440758e9d99d1bd9d93bccd647369
Ronak Manglani <ronak.manglani@gmail.com>
2020-08-16 22:54:38 +0530

    make lives management and show game over on 0

---
+++ Assets/Scripts/RecycleLevel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RecycleLevel : MonoBehaviour
+{
+    public GameObject gameOver;
+    void Start()
+    {
+        GlobalLives.livesValue--;
+        if (GlobalLives.livesValue <= 0)
+        {
+            gameOver.SetActive(true);
+        }
+        else
+        {
+            SceneManager.LoadScene("Level001");
+        }
+    }
+}
---
+++ Assets/Scripts/Stats/GlobalLives.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GlobalLives : MonoBehaviour
+{
+    public GameObject livesDisplay;
+    public static int livesValue = 3;
+    public int internalLives;
+
+    void Update()
+    {
+        internalLives = livesValue;
+        livesDisplay.GetComponent<Text>().text = "" + livesValue;
+    }
+}
