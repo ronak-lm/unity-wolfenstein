899973ab809a5926c22722cdeab04b9f42c37160
Ronak Manglani <ronak.manglani@gmail.com>
2020-08-16 20:52:22 +0530

    make global health script and add health to ui

---
+++ Assets/Scripts/Stats/GlobalHealth.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GlobalHealth : MonoBehaviour
+{
+    public GameObject healthDisplay;
+    public static int healthValue;
+    public int internalHealth;
+
+    void Start()
+    {
+        healthValue = 100;
+    }
+
+    void Update()
+    {
+        internalHealth = healthValue;
+        healthDisplay.GetComponent<Text>().text = "" + healthValue + "%";
+    }
+}
