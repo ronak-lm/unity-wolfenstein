851fd7e6748108719e4411db2beb520317f00e3c
Ronak Manglani <ronak.manglani@gmail.com>
2020-08-16 23:54:52 +0530

    make gold pickup script and global score script

---
+++ Assets/Scripts/Stats/GlobalScore.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GlobalScore : MonoBehaviour
+{
+    public GameObject scoreDisplay;
+    public static int scoreValue = 0;
+    public int internalScore;
+
+    void Update()
+    {
+        internalScore = scoreValue;
+        scoreDisplay.GetComponent<Text>().text = "" + scoreValue;
+    }
+}
