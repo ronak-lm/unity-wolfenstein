3e35316700309bbfde12345576da894adf246406
Ronak Manglani <ronak.manglani@gmail.com>
2020-08-17 00:25:10 +0530

    make full health pickup script

---
+++ Assets/Scripts/Environment/FullHealthCollect.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FullHealthCollect : MonoBehaviour
+{
+    public AudioSource collectSound;
+
+    void OnTriggerEnter(Collider other)
+    {
+        GlobalHealth.healthValue = 100;
+        collectSound.Play();
+        GetComponent<BoxCollider>().enabled = false;
+        this.gameObject.SetActive(false);
+    }
+}
