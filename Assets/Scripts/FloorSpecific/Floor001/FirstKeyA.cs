a41e6a494b345df108b35333454e059c11e4df75
Ronak Manglani <ronak.manglani@gmail.com>
2020-08-18 00:54:41 +0530

    make locked door and key pickup

---
+++ Assets/Scripts/FloorSpecific/Floor001/FirstKeyA.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstKeyA : MonoBehaviour
+{
+    public GameObject theKey;
+    public GameObject keyUI;
+    public GameObject lockedTrigger;
+
+    void OnTriggerEnter(Collider other)
+    {
+        theKey.SetActive(false);
+        keyUI.SetActive(true);
+        lockedTrigger.SetActive(true);
+        GetComponent<BoxCollider>().enabled = false;
+    }
+}
