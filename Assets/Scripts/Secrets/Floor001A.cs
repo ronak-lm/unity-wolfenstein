e97f97e1cc989d5044c6302eaebe82302908eda2
Ronak Manglani <ronak.manglani@gmail.com>
2020-08-17 23:56:16 +0530

    create secret area in level 1

---
+++ Assets/Scripts/Secrets/Floor001A.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Floor001A : MonoBehaviour
+{
+    public GameObject moveableWall;
+
+    void OnTriggerEnter(Collider other)
+    {
+        moveableWall.GetComponent<Animator>().enabled = true;
+        GetComponent<BoxCollider>().enabled = false;
+    }
+}
