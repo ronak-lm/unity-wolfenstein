e042f713f40f68aee8086fa81667070480d4e64d
Ronak Manglani <ronak.manglani@gmail.com>
2020-08-21 01:50:10 +0530

    add health pickup in secret area

---
+++ Assets/Scripts/Environment/ExtraLife.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExtraLife : MonoBehaviour
+{
+    public AudioSource lifePickupSound;
+
+    void OnTriggerEnter(Collider other)
+    {
+        GlobalLives.livesValue += 1;
+        lifePickupSound.Play();
+        this.gameObject.SetActive(false);
+    }
+}
