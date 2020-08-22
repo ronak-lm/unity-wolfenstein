43b13d7ad6efc499450dafe2a7cdc7e7668b1a74
Ronak Manglani <ronak.manglani@gmail.com>
2020-08-23 00:16:22 +0530

    fix hurt flash bug

--- Assets/Scripts/Enemies/EnemyDeath.cs
+++ Assets/Scripts/Enemies/EnemyDeath.cs
@@ -6,6 +6,7 @@ public class EnemyDeath : MonoBehaviour
 {
     public GameObject theEnemy;
     public GameObject enemyAI;
+    public GameObject hurtFlash;
     public int enemyHealth = 20;
     public bool enemyDead = false;
 
@@ -20,6 +21,7 @@ public class EnemyDeath : MonoBehaviour
                 theEnemy.GetComponent<Animator>().Play("Death");
                 theEnemy.GetComponent<LookPlayer>().enabled = false;
                 enemyAI.SetActive(false);
+                hurtFlash.SetActive(false);
 
                 GlobalScore.scoreValue += 100;
                 GlobalComplete.enemyCount++;
