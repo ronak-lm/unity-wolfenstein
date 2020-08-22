985e093e346358c24ff7fe75abff67f1a117c0b3
Ronak Manglani <ronak.manglani@gmail.com>
2020-08-22 23:51:43 +0530

    create main menu ui

---
+++ Assets/Scripts/Menu/MainMenuController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    public AudioSource clickSound;
+    public GameObject fadeOut;
+
+    public void NewGame()
+    {
+        StartCoroutine(NewGameRoutine());
+    }
+
+    IEnumerator NewGameRoutine()
+    {
+        clickSound.Play();
+        fadeOut.SetActive(true);
+        yield return new WaitForSeconds(2);
+        SceneManager.LoadScene("Level001");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
